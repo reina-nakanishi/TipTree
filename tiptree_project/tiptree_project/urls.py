fa56bcfe1fd1461f10af928ae4a859962e0111c4
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-08-31 17:55:29 +0900

    ログイン画面用のurl作成

--- tiptree_project/tiptree_project/urls.py
+++ tiptree_project/tiptree_project/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.portfolio, name='portfolio')
+    path('', views.portfolio, name='portfolio'),
+    path('accounts/',include('acounts.urls')),
 ]
