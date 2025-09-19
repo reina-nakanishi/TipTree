394415dde49e86d1a022b1b3fe103abeb8afa9c3
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-09-19 12:13:54 +0900

    registを定義

--- tiptree_project/accounts/views.py
+++ tiptree_project/accounts/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
-def login(request):
-    return render(request,"login.html")
+
+def regist(request):
+    regist_form = forms.RegistForm(request.POST or None)
+    if regist_form.is_valid():
+        return redirect('accounts:confirm.html')
+    return render(request,"accounts/regist.html",context={
+        'regisst_form':regist_form
+    })
+
+def login_view(request):
+    form = forms.LoginForm
+    return render(
+        request,"accounts/login.html",context={
+            'form': form
+        })
