4e42899469f0fabcf64d3c2c7f00ddc9a87e18c2
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-10-02 12:01:38 +0900

    tiptree_project/urls.pyとhome/urls.pyを紐づけ

--- tiptree_project/tiptree_project/urls.py
+++ tiptree_project/tiptree_project/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.portfolio, name='portfolio'),
-    path('accounts/',include('acounts.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('home',include('home.urls')),
 ]
