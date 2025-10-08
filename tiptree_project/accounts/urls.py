cbcc204ad19e9b25011c2e20ca22bbeaafbc2898
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-10-08 17:39:56 +0900

    logoutのurlを作成

--- tiptree_project/accounts/urls.py
+++ tiptree_project/accounts/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('regist', views.regist, name = 'regist'),
+    path('confirm', views.confirm, name = 'confirm'),
     path('login', views.login_view, name = 'login'),
+    path('logout', views.logout_view, name = 'logout'),
+    path('help', views.help, name = 'help'),
+    path('my_page', views.my_page, name = 'my_page'),
 ]
