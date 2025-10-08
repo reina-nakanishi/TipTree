53dedb75c9879a989e7f93503b421fbc36729c69
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-10-08 17:45:54 +0900

    homeのurlを作成

--- tiptree_project/tiptree_project/urls.py
+++ tiptree_project/tiptree_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.portfolio, name='portfolio'),
     path('accounts/',include('accounts.urls')),
-    path('home',include('home.urls')),
+    path('home/',include('home.urls')),
 ]
