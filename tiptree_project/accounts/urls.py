b745ac7f98aa225966e71045416127ecb08f65bf
“reina-nakanishi” <reisyoko1214@gmail.com>
2025-10-18 21:07:39 +0900

    password_reset,password_reset_done,password_reset_confirmのurlを追加

--- tiptree_project/accounts/urls.py
+++ tiptree_project/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import CustomPasswordResetView,CustomPasswordResetDoneView,CustomPasswordResetConfirmView
 
 app_name = 'accounts'
 
@@ -8,7 +10,9 @@ urlpatterns = [
     path('confirm', views.confirm, name = 'confirm'),
     path('login', views.login_view, name = 'login'),
     path('logout', views.logout_view, name = 'logout'),
-    path('help', views.help, name = 'help'),
+    path('password_reset', CustomPasswordResetView.as_view(), name='password_reset'), 
+    path('password_reset/done', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('my_page', views.my_page, name = 'my_page'),
     path('user_edit', views.user_edit, name = 'user_edit'),
     path('change_password', views.change_password, name = 'change_password'),
