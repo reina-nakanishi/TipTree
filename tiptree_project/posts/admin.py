74284430546025bb2423d44fb1aa16e17aa14ecc
“reina-nakanishi” <reisyoko1214@gmail.com>
2026-03-06 19:50:39 +0900

    posts/adminでカテゴリーを管理できるようにした

--- tiptree_project/posts/admin.py
+++ tiptree_project/posts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            kwargs["queryset"] = Category.objects.filter(parent__isnull=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        # 子カテゴリなのに親が未設定ならエラー
+        if obj.parent is None and Category.objects.filter(parent=obj).exists():
+            pass
+        super().save_model(request, obj, form, change)
