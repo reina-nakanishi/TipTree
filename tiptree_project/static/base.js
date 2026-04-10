d8d9f40d32d4e1b57d19b2d48f5296ce52143590
“reina-nakanishi” <reisyoko1214@gmail.com>
2026-04-10 23:09:02 +0900

    base.jsでモーダル機能を追加した

--- tiptree_project/static/base.js
+++ tiptree_project/static/base.js
@@ -8,3 +8,85 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const messages = {
+    post: "この投稿を削除しますか？",
+    comment: "このコメントを削除しますか？",
+    supplement:"この補足を削除しますか？",
+    comment_reply:"この返信を削除しますか？",
+    supplement_reply:"この返信を削除しますか？",
+    logout:"ログアウトしますか？",
+    auth:"この操作にはログインが必要です"
+  };
+
+  document.querySelectorAll("[data-type]").forEach(el => {
+    el.addEventListener("click", (e) => {
+
+      const url = el.dataset.url;
+      const title = el.dataset.title;
+      const type = el.dataset.type;
+
+      e.preventDefault();
+
+      const message = `${messages[type]}\n${title || ""}`;
+
+      openModal({
+        message: message,
+        url: url,
+        type: type
+      });
+
+    });
+  });
+
+});
+
+function openModal({ message, url, type }) {
+  const modal = document.getElementById("common-modal");
+  const messageArea = document.getElementById("modal-message");
+  const form = document.getElementById("modal-form");
+  const actionsArea = document.getElementById("modal-actions");
+
+  // メッセージ表示
+  messageArea.innerText = message;
+
+  // フォームを渡すurlを指定
+  form.action = url;
+
+  // ボタン初期化
+  actionsArea.innerHTML = "";
+
+  // 削除ボタン
+  const buttonTexts = {
+    logout: "ログアウト",
+    auth:"ログイン"
+  };
+
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.innerText = buttonTexts[type] || "削除";
+
+  if (type === "auth") {
+    deleteBtn.className = "primary";
+    deleteBtn.type = "button";
+
+    deleteBtn.addEventListener("click", () => {
+      window.location.href = url;
+    });
+
+  } else {
+    deleteBtn.className = "danger";
+    deleteBtn.type = "submit";
+  }
+
+  actionsArea.appendChild(deleteBtn);
+
+  modal.classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("common-modal").classList.add("hidden");
+}
