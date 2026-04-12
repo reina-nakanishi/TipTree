6044557c04c08251ff09bbfe147340e24e62daa2
“reina-nakanishi” <reisyoko1214@gmail.com>
2026-04-12 19:48:24 +0900

    create_post.jsで動画とサムネイルのプレビュー表示ができるようにした

--- tiptree_project/static/posts/js/create_post.js
+++ tiptree_project/static/posts/js/create_post.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const parentSelect = document.getElementById("parent-category");
   const childSelect = document.getElementById("child-category");
 
-  parentSelect.addEventListener("change", () => {
+  parentSelect?.addEventListener("change", () => {
     const parentId = parentSelect.value;
 
     // 親カテゴリ未選択なら子カテゴリをリセット
     if (!parentId) {
       childSelect.innerHTML =
-        '<option value="">サブカテゴリを選択</option>';
+        '<option value="">---------</option>';
       return;
     }
 
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(data => {
         // 子カテゴリを初期化
         childSelect.innerHTML =
-          '<option value="">サブカテゴリを選択</option>';
+          '<option value="">---------</option>';
 
         // JSONデータを元にoptionを追加
         data.forEach(category => {
@@ -37,4 +37,43 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error("エラー:", error);
       });
   });
+
+
+
+  // ===== サムネ =====
+  const thumbnailInput = document.getElementById("thumbnailInput");
+  const thumbnailPreview = document.getElementById("thumbnailPreview");
+
+  thumbnailInput?.addEventListener("change", () => {
+    const file = thumbnailInput.files[0];
+    if (!file) return;
+
+    // temp / current 両方消す
+    document.querySelectorAll(".temp-thumbnail, .current-thumbnail")
+      .forEach(el => el.remove());
+
+    const url = URL.createObjectURL(file);
+    thumbnailPreview.src = url;
+    thumbnailPreview.style.display = "block";
+  });
+
+
+  // ===== 動画 =====
+  const videoInput = document.getElementById("videoInput");
+  const videoPreview = document.getElementById("videoPreview");
+
+  videoInput?.addEventListener("change", () => {
+    const file = videoInput.files[0];
+    if (!file) return;
+
+    // temp / current 両方消す
+    document.querySelectorAll(".temp-video, .current-video")
+      .forEach(el => el.remove());
+
+    const url = URL.createObjectURL(file);
+    videoPreview.src = url;
+    videoPreview.style.display = "block";
+    videoPreview.setAttribute("controls", "controls");
+  });
+
 });
